49b0df7136e86b18dc8b7a98657a2554037ee9cb
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2023-01-08 16:38:01 +0600

    rtk query video app

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,63 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+    reducerPath: 'api',
+    tagTypes:["Vodeos", "Video", "RelatedVideo"],
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:9000'
+    }),
+    endpoints: (builder) => ({
+        getVideos: builder.query({
+            query: () => "/videos",
+            keepUnusedDataFor: 600,
+            providesTags:["Videos"]
+        }),
+        getVideo: builder.query({
+            query: (videoId) => `/videos/${videoId}`,
+            providesTags:(result, error, arg) => [
+                {type:"Video", id:arg},
+            ]
+        }),
+        //?title_like=css&title_like=tailwind&_limit=4
+        getRelatedVideos: builder.query({
+            query: ({ id, title }) => {
+                const tags = title.split(" ");
+                const likes = tags.map((tag) => `title_like=${tag}`);
+                const queryString = `/videos?${likes.join("&")}&_limit=4`;
+                return queryString;
+            },
+            providesTags:(result, error, arg) => [
+                {type:"RelatedVideo", id:arg.id},
+            ]
+        }),
+        addVideo: builder.mutation({
+            query: (data) => ({
+                url: "/videos",
+                method: "POST",
+                body: data,
+            }),
+            invalidatesTags:["Videos"]
+        }),
+        editVideo: builder.mutation({
+            query: ({id, data}) => ({
+                url: `/videos/${id}`,
+                method: "PATCH",
+                body: data,
+            }),
+            invalidatesTags: (result, error, arg) => [
+                "Videos",
+                {type:"Video", id:arg.id},
+                {type:"RelatedVideo", id:arg.id},
+            ]
+        }),
+        deleteVideo: builder.mutation({
+            query: (id) => ({
+                url: `/videos/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["Videos"],
+        }),
+    })
+});
+
+export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery, useAddVideoMutation, useEditVideoMutation, useDeleteVideoMutation } = apiSlice;
